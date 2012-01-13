1660bd9a4f91f88307c10e85b7f3f82442c800a4
Ray Brownrigg <Ray.Brownrigg@ecs.vuw.ac.nz>
2012-01-13 08:11:38 +0000

    version 2.2-1

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,3 @@
 .onLoad <- function(lib, pkg) {
-  if(!exists("Sys.setenv", envir = baseenv()))
-    Sys.setenv <- Sys.putenv
   Sys.setenv("R_MAPDATA_DATA_DIR"=paste(lib, pkg, "mapdata/", sep="/"))
 }
